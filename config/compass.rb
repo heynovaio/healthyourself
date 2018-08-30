9148db50069e335abd968be8d255913eec5f13a1
Kirsten Dodd <kirsten.jvd@gmail.com>
2018-08-30 13:07:20 -0700

    First round of style changes

---
+++ config/compass.rb
@@ -0,0 +1,20 @@
+css_dir = "assets"
+sass_dir = "assets/sass"
+javascripts_dir = "assets/js"
+images_dir = "assets/images"
+
+sourcemap = false
+output_style = :compressed
+
+# Saves css files as liquid
+on_stylesheet_saved do |filename|
+  if File.exists?(filename)
+    # Break up the path
+    path = File.dirname(filename) + "/"
+    file = File.basename(filename, ".*")
+    extension = ".scss.liquid"
+
+    # Move the file to new location
+    FileUtils.mv filename, path + file + extension
+  end
+end
